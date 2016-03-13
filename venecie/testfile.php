fe45c65433326626169b24c493e161e94433a3f3
Shelyakin Anton <gordonshamuelle@Gmail.com>
2016-03-14 02:15:55 +0300

    ап верстки
    обавлены текста
    другие фотки

---
+++ venecie/testfile.php
@@ -0,0 +1,22 @@
+<?php
+
+$name=$_GET['c1'];
+$mail=$_GET['c2'];
+$number=$_GET['c3'];
+$time=$_GET['c4'];
+$guests=$_GET['c5'];
+$date=$_GET['c6'];
+$arrival=$_GET['c7'];
+$advance=$_GET['c8'];
+
+
+echo $name;
+echo $mail;
+echo $number;
+echo $time;
+echo $guests;
+echo $date;
+echo $arrival;
+echo $advance;
+
+?>
